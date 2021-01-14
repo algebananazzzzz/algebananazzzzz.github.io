68a78851385946ff6103666e6280c7d7172d3370
algebananazzzzz <daniel.zhouqx@gmail.com>
2021-01-14 12:11:26 +0800

    initial commit

---
+++ js/home.js
@@ -0,0 +1,21 @@
+
+
+$('.canvas-basic').each(function() {
+  new Granim({
+    element: this,
+    direction: 'left-right',
+    states: {
+      "default-state": {
+        gradients: [
+          ['#F45C43', '#EB3349'],
+          ['#FF8008', '#FFC837'],
+          ['#3CD3AD', '#4CB8C4'],
+          ['#24C6DC', '#514A9D'],
+          ['#DD2476', '#FF512F'],
+          ['#DA22FF', '#9733EE'],
+        ],
+        transitionSpeed: 5000
+      }
+    }
+  });
+});
